3edbc334040fc4d7d2f3c18617510025828d0a4f
pritom rabidas <pritomrabidas102@gmail.com>
2025-04-02 11:33:36 +0600

    cake

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-Nunito-font text-sm text-secandari'>hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#212121",
+        secandari: "#289c28",
+        brand: "#9b9fa5",
+      },
+      fontFamily: {
+        Popins: ["Poppins", "sans-serif"],
+        NunitoFont: ["Nunito", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
+        Raleway: ["Raleway", "sans-serif"],
+        Opensans: ["Open Sans", "sans-serif"],
+        Montez: ["Montez", "serif"],
+        Monrope: ["Manrope", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
