581381247e6a24fb89e48394c167a07363479467
pritom rabidas <pritomrabidas102@gmail.com>
2025-04-23 13:54:33 +0600

    register error part

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -6,6 +6,37 @@ import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
 const Register = () => {
   const [showPass, setShowPass] = useState("");
+  const [userdata, setUserdata] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [userdataErr, setUserdataErr] = useState({
+    nameErr: "",
+    emailErr: "",
+    passwordErr: "",
+  });
+  const HandleSubmit = () => {
+    if (!userdata.name) {
+      setUserdataErr((prev) => ({ ...prev, nameErr: "Name is required" }));
+    } else if (!userdata.email) {
+      setUserdataErr((prev) => ({ ...prev, emailErr: "Email is required" }));
+    } else if (!userdata.password) {
+      setUserdataErr((prev) => ({
+        ...prev,
+        passwordErr: "Password is required",
+      }));
+    } else {
+      console.log("From submited successfully", userdata);
+      setUserdataErr({
+        nameErr: "",
+        emailErr: "",
+        passwordErr: "",
+      });
+    }
+      
+  };
+
   return (
     <div className="">
       <div className="flex py-4 bg-gray-50 h-screen pt-24">
@@ -15,26 +46,38 @@ const Register = () => {
               <div className="">
                 <input
                   type="text"
+                  onChange={(e) =>
+                    setUserdata((prev) => ({ ...prev, name: e.target.value }))
+                  }
                   className="outline-none border px-3 py-2 rounded-md text-sm text-primary font-normal font-Opensans w-[80%]"
                   placeholder="Full Name"
                 />
                 <p className=" font-NunitoFont font-normal text-sm text-red-700 py-0.5">
-                  error
+                  {userdataErr.nameErr}
                 </p>
               </div>
               <div>
                 <input
+                  onChange={(e) =>
+                    setUserdata((prev) => ({ ...prev, email: e.target.value }))
+                  }
                   type="email"
                   className="outline-none px-3 py-2 rounded-md text-sm text-primary font-normal font-Opensans border w-[80%]"
                   placeholder="Email"
                 />
                 <p className="font-NunitoFont font-normal text-sm text-red-700 py-0.5">
-                  error
+                  {userdataErr.emailErr}
                 </p>
               </div>
               <div>
                 <div className="flex items-center justify-end relative w-[80%]">
                   <input
+                    onChange={(e) =>
+                      setUserdata((prev) => ({
+                        ...prev,
+                        password: e.target.value,
+                      }))
+                    }
                     type={showPass ? "text" : "Password"}
                     className="outline-none px-3 py-2 rounded-md text-sm text-primary font-normal font-Opensans border w-full justify-start"
                     placeholder="Password"
@@ -47,15 +90,20 @@ const Register = () => {
                   </div>
                 </div>
                 <p className="font-NunitoFont font-normal text-sm text-red-700 py-0.5">
-                  error
+                  {userdataErr.passwordErr}
                 </p>
               </div>
-              <button className="text-base font-Monrope font-medium text-primary bg-secandari px-5 py-1.5 rounded-2xl cursor-pointer hover:bg-secandari/90 duration-200 mt-4 w-[80%]">
+              <button
+                onClick={HandleSubmit}
+                className="text-base font-Monrope font-medium text-primary bg-secandari px-5 py-1.5 rounded-2xl cursor-pointer hover:bg-secandari/90 duration-200 mt-4 w-[80%]"
+              >
                 Register
               </button>
             </div>
             <div className="pt-6">
-              <p className="text-base text-primary font-normal font-Monrope">Or, log in with</p>
+              <p className="text-base text-primary font-normal font-Monrope">
+                Or, log in with
+              </p>
               <div className="flex items-center gap-4 pt-5">
                 <Link>
                   <FcGoogle className="text-2xl cursor-pointer" />
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -9,13 +9,27 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      state.product.push(action.payload);
-      localStorage.setItem("product", JSON.stringify(state.product));
+      const existingProduct = state.product.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProduct) {
+        existingProduct.quantity =
+           action.payload.quantity;
+        existingProduct.allPrice =
+          existingProduct.quantity * existingProduct.price;
+        localStorage.setItem("product", JSON.stringify(state.product));
+      } else {
+        state.product.push(action.payload);
+        localStorage.setItem("product", JSON.stringify(state.product));
+      }
     },
     removeFromCart: (state, action) => {
-      state.product = state.product.filter((item)=> item.id !== action.payload);
-    }
-  }
+      state.product = state.product.filter(
+        (item) => item.id !== action.payload
+      );
+      localStorage.setItem("product", JSON.stringify(state.product));
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
